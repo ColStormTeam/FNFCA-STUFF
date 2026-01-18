e3a5a259ae3b4b1072d94fb8f1b37ff1cec52d0e
ItzRanbins <zdgsokol1@gmail.com>
2026-01-18 15:15:19 +0300

    part 3

---
+++ Docs Scripts/Modifiers List and Info.hx
@@ -0,0 +1,338 @@
+Modifier List:
+
+- Random Modifier (+10%)
+- 3 Random Modifiers (+30%)
+- 5 Random Modifiers (+50%)
+
+- Ghost Tapping (-10%)
+- Low Speed (-10%)
+- Lowest Speed (-30%)
+- No Death (-20%)
+- 100% Health Start (-20%)
+- More Coin Notes (-30%)
+- No Mechanics (-50%)
+- 3K+Random (-5%)
+- 2K+Random (-10%)
+- 1K+Random (-20%)
+
+- High Speed (+10%)
+- HIGHER Speed (+20%)
+- MAXIMUM SPEED (+50%)
+- Low Health Drain (+5%)
+- High Health Drain (+15%)
+- Low HP: Increase Speed (+5%)
+- Slow Health Gain (+5%)
+- Fast Health Loss (+10%)
+- Only "Epic" (+30%)
+- 5% Heath Start (+20%)
+- 50% MAX Health (+10%)
+- Flip Notes (+10%)
+- Random Notes (+20%)
+- .jpeg Fun (+50%)
+- Half Note Appear (+15%)
+- Half Note Disappear (+5%)
+- Random Strums Speed (+20%)
+- Mania Mode (+50%)
+- Less Coin Notes (+5%)
+- No Coin Notes (+10%)
+- More Mechanics (+20%)
+
+
+- 5K+Random (+20%)
+- 6K+Random (+50%)
+- 7K+Random (+70%)
+- 8K+Random (+100%)
+- 9K+Random (+120%)
+
+- Practice Mode (NO SCORES/NO SAVE)
+- Botplay (NO MODIFIERS/NO SCORES/NO SAVE)
+
+(The order of modifiers laid out here will also be laid out in the game.)
+
+
+Name: Small Justice
+Description: Activates a random modifier of any type, even if you have some modifiers locked.
+HEX Color 0xf8e74e
+
+Bonus: +10%
+How to unlock: [Initially available]
+
+Name: Random Badness
+Description: Activates 3 random modifiers of any type, even if you have some modifiers locked.
+HEX Color 0xec9b31
+
+Bonus: +30%
+How to unlock: [Initially available]
+
+Name: Tricky Troubles
+Description: Activates 5 random modifiers of any type, even if you have some modifiers locked.
+HEX Color 0xf37725
+
+Bonus: +50%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Phantom Earnings
+Description: If you hit blank notes, the game wont count the error ("Misses"). Seems like you are so into this lame shit? Bruh...
+HEX Color 0x3eb61a
+
+Bonus: -10%
+How to unlock: [Initially available]
+
+Name: Slow Happiness
+Description: The speed of the notes will be slower. Your consciousness may not be able to respond at a reasonably normal speed...
+HEX Color 0x61d12d
+
+Bonus: -10%
+How to unlock: [Initially available]
+
+Name: Delayed Smartass
+Description: The speed of the notes will become more slow... Are your reactions really that sluggish?
+HEX Color 0x1ebb4d
+
+Bonus: -30%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Nifty Сatch
+Description: The speed of the notes will go a little faster than normal. Is your mind so bold that you can react to THAT!?
+HEX Color 0xddcc34
+
+Bonus: +10%
+How to unlock: Unlock the badge: [Initially available]
+
+Name: Faster Than Bullets
+Description: The speed of the notes will go WAY faster than normal! DID YOU OVERPLAY GEOMETRY DASH BY BEATING EXTREME DEMONS!?
+HEX Color 0xbb5d1e
+
+Bonus: +20%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: FASTER THAN THE MIND
+Description: YOUR NOTES WILL TURN INTO FUCKING SONIC!!!
+HEX Color 0x1e7cbb
+
+Bonus: +50%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Immortal Desire
+Description: You will not die, for the master god has decided to reward you with undying immortality... A dishonorable immortality.
+HEX Color 0x29d671
+
+Bonus: -20%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Healthier Than Ever
+Description: Your health bar will start right at 100%. Your opponent will envy you for being so lucky.
+HEX Color 0x20b67c
+
+Bonus: -20%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Barely Keeping Up
+Description: Your health scale will start at 5%. It seems that while you were at the beginning of the song, Light managed to stab you with a knife out of anger, but you are still standing.
+HEX Color 0xf7813d
+
+Bonus: +15%
+How to unlock: [Initially available]
+
+Name: Half Bitten
+Description: Your health scale will MAX OUT at 50%. The Limit has decided to play a cruel trick on you.
+HEX Color 0xcc4b4b
+
+Bonus: +10%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Musical Freakout
+Description: The less health you have, the faster the notes will move.... GO AWAY CLOWNS!
+HEX Color 0xf88b61
+
+Bonus: +5%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Malignant Immunity
+Description: Your health bar will increase very slowly as you hit the beat of the note. Now your health has turned into a rotten snail.
+HEX Color 0xf0cc2c
+
+Bonus: +5%
+How to unlock: [Initially available]
+
+Name: Justice Unfathomable
+Description: During your opponent turn, your health will be used up slower than normal. If your health scale reaches zero, you are automatically Blueballed. In the normal case, the motivation of the ColStorm team seniors is also wasted.
+HEX Color 0xee811a
+
+Bonus: +5%
+How to unlock: [Initially available]
+
+Name: An Immunity Foe
+Description: During your opponent turn, your health will be used up faster than normal. If your health bar reaches zero, you automatically get "Blueballed". Just like motivation for drawing sprites from ОРАНГЕ.
+HEX Color 0xff460e
+
+Bonus: +15%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Cursed Error
+Description: If you make a mistake or miss, your health scale will be lost much faster than normal. This is how FNF newbies imagine their true fear in a speedrun.
+HEX Color 0xeb3a3a
+
+Bonus: +10%
+How to unlock: [Initially available]
+
+Name: Epic PERFECTIONism
+Description: You only need to hit "Epic" combos. Otherwise, you will immediately get "Blueballed". Now imagine what it feels like to correct your grades in school......
+HEX Color 0xebae2b
+
+Bonus: +30%
+How to unlock: [Initially available]
+
+Name: WrOnG NoTeS
+Description: The notes in the song would swap places, creating an awkward passage. Hmmm... I should try an experiment on swapping "bread and butter" notes..... That sounds kind of ridiculous...
+HEX Color 0xda9e1f
+
+Bonus: +10%
+How to unlock: [Initially available]
+
+Name: Mystery Of Intelligence
+Description: The notes will be scattered randomly.... Now the question is: Did AI get to FNF Colors Adventure, or did some naughty kids make a mess?
+HEX Color 0x4a10b4
+
+Bonus: +20%
+How to unlock: [Initially available]
+
+Name: Ghost Dance
+Description: The notes only appear halfway down the screen, so you may not have time to react... The spies will be totally screwed...
+HEX Color 0xb1ce11
+
+Bonus: +15%
+How to unlock: [Initially available]
+
+Name: Notes - The Impostors
+Description: The notes only appear halfway down the screen, so blind squirrels can be pretty dumb....
+HEX Color 0xf1af21
+
+Bonus: +5%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Spoiled Hypnotists
+Description: Your notes will rush at random speeds.... Although we think virus particles are still flying....
+HEX Color 0xecd342
+
+Bonus: +20%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Mania Mode
+Description: This modifier changes the difficulty to FUNky! Remember what songs were in osu!mania?
+HEX Color 0xaf0c86
+
+Bonus: +50%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Master Burglar
+Description: Coin Notes will be showing up more than usual.
+HEX Color 0x29b90d
+
+Bonus: -30%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Coin Hater
+Description: Coin Notes will appear less than usual.
+HEX Color 0xd5b200
+
+Bonus: +5%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: No Rich Glutton
+Description: Coin Note вообще не будут появляться.
+HEX Color 0xf36e1e
+
+Bonus: +10%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Cowardly Mechanic
+Description: Game mechanics will not show up in the song, and neither your cowardly ass.
+HEX Color 0x20d36b
+
+Bonus: -50%
+How to unlock: [Initially available]
+
+Name: In Search of Doubling
+Description: Game mechanics will appear more often than usual.
+HEX Color 0xa8cf18
+
+Bonus: +20%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Disgusting Streaming Connection
+Description: HEY, BRO! ENLARGE THE FUCKING WINDOW AND BUY SOME GODDAMN BASS HEADPHONES!
+HEX Color 0x18c574
+
+Bonus: +50%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: One Button Nerd
+Description: The modifier includes a 1K mode as well as built-in Random Notes. How long can you only press one damn button HUH!?
+HEX Color 0x1c62ef
+
+Bonus: -20%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Two and Two
+Description: The modifier includes a 2K mode as well as built-in Random Notes.
+HEX Color 0x2719f0
+
+Bonus: -10%
+How to unlock: Unlock the badge: "Experimental Feedback"
+
+Name: Triple Cycle
+Description: The modifier includes a 3K mode as well as built-in Random Notes.
+HEX Color 0x7b16f2
+
+Bonus: -5%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Fivefold Insanity
+Description: The modifier includes a 5K mode as well as built-in Random Notes. Umm... A Triple Trouble moment, but instead of a ring of DAMN ROMB?
+HEX Color 0xd312f3
+
+Bonus: +20%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Six Flashback Pieces
+Description: The modifier includes a 6K mode as well as built-in Random Notes. Are you at least willing to go through a 6 arrow song if the players hate it?
+HEX Color 0xf50fba
+
+Bonus: +50%
+How to unlock: Unlock the badge: "Iridescent Overkill"
+
+Name: Sevenfold Stiffness Wheel
+Description: The modifier includes a 7K mode as well as built-in Random Notes.
+HEX Color 0xf60c5d
+
+Bonus: +70%
+How to unlock: Unlock the badge: "Infernal Deity"
+
+Name: Eightfold Agony of Death
+Description: The modifier includes a 8K mode as well as built-in Random Notes.
+HEX Color 0xf71408
+
+Bonus: +100%
+How to unlock: Unlock the badge: "Infernal Deity"
+
+Name: The Fatal Nine
+Description: The modifier includes a 9K mode as well as built-in Random Notes. Что ж... Развлекайтесь, пока можете, мелкие просители прямиком в АД.....
+HEX Color 0xf87206
+
+Bonus: +150%
+How to unlock: Unlock the badge: "Infernal Deity"
+
+Name: Practice Mode
+Description: If you want to practice, Practice Mode is for you, but your stats and points will not be saved!
+HEX Color 0xb1b1b1
+
+Bonus: NO SCORES/NO SAVE
+How to unlock: [Initially available]
+
+Name: Botplay
+Description: ... There is no point in explaining... Modifiers will not work, and points and stats will not be saved.
+HEX Color 0xb1b1b1
+
+Bonus: NO MODIFIERS/NO SCORES/NO SAVE
+How to unlock: [Initially available]
